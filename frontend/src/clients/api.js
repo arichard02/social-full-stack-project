bd3fdba22ab63a467d4cea449886b04f8078933d
Adwaina <arichard02@aol.com>
2026-03-26 18:38:33 -0400

    addede base url

--- frontend/src/clients/api.js
+++ frontend/src/clients/api.js
@@ -6,14 +6,14 @@ console.log(BASE_URL)
 export const token = () => localStorage.getItem('token')
 
 export const userClient = axios.create({
-     baseURL: 'http://localhost:3000/api/users',
+     baseURL: BASE_URL + '/api/users',
      headers: {
           Authorization: `Bearer ${token()}`
      }
 })
 
 export const postClient = axios.create({
-     baseURL: 'http://localhost:3000/api/posts'
+     baseURL: BASE_URL + '/api/posts'
 })
 
 // use the latest version of the token in local storage
