664ae9b8149620f6851992098c1b85018ddb914a
Adwaina <arichard02@aol.com>
2026-03-28 00:18:42 -0400

    added populate and used it to return username of the author for post

--- frontend/src/clients/api.js
+++ frontend/src/clients/api.js
@@ -1,23 +1,23 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL
-console.log(BASE_URL)
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+console.log(BASE_URL);
 
-export const token = () => localStorage.getItem('token')
+export const token = () => localStorage.getItem("token");
 
 export const userClient = axios.create({
-     baseURL: BASE_URL + '/api/users',
-     headers: {
-          Authorization: `Bearer ${token()}`
-     }
-})
+  baseURL: BASE_URL + "/api/users",
+  headers: {
+    Authorization: `Bearer ${token()}`,
+  },
+});
 
 export const postClient = axios.create({
-     baseURL: BASE_URL + '/api/posts'
-})
+  baseURL: BASE_URL + "/api/posts",
+});
 
 // use the latest version of the token in local storage
 postClient.interceptors.request.use((req) => {
-     if (token()) req.headers.Authorization = `Bearer ${token()}`
-     return req
-})
+  if (token()) req.headers.Authorization = `Bearer ${token()}`;
+  return req;
+});
